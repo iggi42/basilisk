64e8b530a38f942cba7f48aaac1b266f89611a43
Ig <iggi@m3lt.work>
2021-02-22 05:12:06 +0100

    fix my tooling setup

--- tools/direnv/manifest.scm
+++ tools/direnv/manifest.scm
@@ -1,4 +1,5 @@
-(use-modules (gnu packages))
+(use-modules (gnu packages)
+             (gnu packages elixir))
 
 (specifications->manifest
- '("bash" "elixir" "git" "nss-certs" "protobuf" ) )
+ '("bash" "elixir" "git" "nss-certs" "protobuf" "make" ) )
