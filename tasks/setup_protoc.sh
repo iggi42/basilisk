07a0690bde1e17f45c909a2af3d4d7a9b3fc8b0e
Ig <iggi@m3lt.work>
2021-02-23 15:55:15 +0100

    better bash script

--- tasks/setup_protoc.sh
+++ tasks/setup_protoc.sh
@@ -3,20 +3,13 @@ set -e
 
 MIX_HOME="$(elixir -e 'Mix.Utils.mix_home() |> IO.puts()')"
 
-function which() {
-  which $@ 1>/dev/null 2>&1
-  return $?
-}
-
-if which asdf; then
+if type asdf &>/dev/null; then
   echo "Reshimming asdf..."
   echo "If warnings about masking executables in your path appeared, you can safely ignore them."
   asdf reshim
 fi
 
-if  which protoc-gen-elixir; then
-  true
-else
+if ! type protoc-gen-elixir &>/dev/null; then
   echo "<<===========----------=============>>" >&2
   echo "WARNING: to complete the setup add '$MIX_HOME/escripts' to your PATH!" >&2
   echo "<<===========----------=============>>" >&2
